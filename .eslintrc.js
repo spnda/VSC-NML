600df4871be4a8c60acd241927de0eb7632eb71d
spnda <43609023+spnda@users.noreply.github.com>
2021-06-15 18:55:00 +0200

    Use ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "parser": "@typescript-eslint/parser",
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+    ],
+};
